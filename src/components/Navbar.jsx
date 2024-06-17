ffbb7a4213ea73c7679b981c6815efdd377f4316
SerDunk <abhinavkondapalli7@gmail.com>
2024-06-17 23:48:07 +0530

    Using Framer Motion library to add basic animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
+import { motion, spring } from "framer-motion";
 
 function Navbar() {
   return (
     <div className="navbar bg-main flex justify-between p-4">
-      <div className="cursor-pointer">
+      <motion.div
+        initial={{ y: "-100vh" }}
+        animate={{ y: 0 }}
+        transition={{ delay: 0.3 }}
+        className="cursor-pointer"
+      >
         <Link
           to="/"
           className="ml-3 text-4xl font-primary font-extrabold text-second p-2 hover-all btn-animation"
         >
-          ToDoist
+          ToDoodle
         </Link>
-      </div>
-      <div className="flex">
+      </motion.div>
+      <motion.div
+        initial={{ y: "-100vh" }}
+        animate={{ y: 0 }}
+        transition={{ delay: 0.3, type: "tween" }}
+        className="flex"
+      >
         <Link to="/todo">
           <button className="btn mr-5 text-second hover-all btn-animation">
             Get Started
           </button>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,25 @@
 import Scatter from "../images/circle-scatter-haikei.svg";
-
+import { motion } from "framer-motion";
 function HomePage() {
   return (
     <div
       style={{ backgroundImage: `url(${Scatter})` }}
       className="bg-cover min-h-screen flex justify-center items-center"
     >
-      <div>
-        <h1 className="text-5xl text-second">The platform for Todoing</h1>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1.5 }}
+        className="z-10"
+      >
+        <motion.h1 className="text-5xl text-second">
+          <span className="hover-all p-2 cursor-pointer border-radius">
+            One
+          </span>
+          {""}
+          platform for To Do
+        </motion.h1>
+      </motion.div>
     </div>
   );
 }
